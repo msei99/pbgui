08955f59cc37c38189d28c0a5f696fb240822255
Manfred Seiler <mani@msei.ch>
2023-11-01 23:51:41 +0100

    v0.6

--- pages/4_Live.py
+++ pages/4_Live.py
@@ -1,38 +1,10 @@
 import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
 from streamlit_autorefresh import st_autorefresh
-from pbgui_func import set_page_config
+from pbgui_func import set_page_config, upload_pbconfigdb
 from Instance import Instances, Instance
-from User import Users
 import pbgui_help
 
-# Cleanup session_state
-def cleanup():
-    if "view_instance" in st.session_state:
-        del st.session_state.view_instance
-    if "edit_instance" in st.session_state:
-        del st.session_state.edit_instance
-
-# handler for button clicks
-def button_handler(button=None):
-    if button == "back_instance":
-        cleanup()
-    if button == "back_history":
-        del st.session_state.view_history
-    if button == "back_edit_instance":
-        cleanup()
-        del st.session_state.pbgui_instances
-    if button == "add_instance":
-        cleanup()
-        st.session_state.edit_instance = Instance()
-    if button == "view_history":
-        st.session_state.view_history = True
-    if button == "save_instance":
-        st.session_state.edit_instance.save()
-        del st.session_state.edit_instance
-        del st.session_state.pbgui_instances
-    if button == "update_symbols":
-        st.session_state.edit_instance.update_symbols()
 
 def select_instance():
     instances = st.session_state.pbgui_instances
@@ -55,7 +27,7 @@ def select_instance():
             'Market_type': instance.market_type,
             'Delete': False,
         })
-    selected = st.data_editor(data=d, width=None, height=None, use_container_width=True, key="editor_select_instance", hide_index=None, column_order=None, column_config=column_config, disabled=['id','User','Symbol','Market_type'])
+    selected = st.data_editor(data=d, width=None, height=1024, use_container_width=True, key="editor_select_instance", hide_index=None, column_order=None, column_config=column_config, disabled=['id','User','Symbol','Market_type'])
     for line in selected:
         if line["View"]:
             st.session_state.view_instance = instances.instances[line["id"]]
@@ -86,8 +58,12 @@ def view_instance():
     instance = st.session_state.view_instance
     # Navigation
     with st.sidebar:
-        st.button(":back:", key="back_instance", on_click=button_handler, args=["back_instance"])
-        st.button("History", key="history", on_click=button_handler, args=["view_history"])
+        if st.button(":back:"):
+            del st.session_state.view_instance
+            st.experimental_rerun()
+        if st.button("History"):
+            st.session_state.view_history = True
+            st.experimental_rerun()
     col_tf, col_auto, col_rec, col_empty = st.columns([3,3,2,10])
     with col_rec:
         st.write("## ")
@@ -105,7 +81,9 @@ def view_history():
     instance = st.session_state.view_instance
     # Navigation
     with st.sidebar:
-        st.button(":back:", key="back_history", on_click=button_handler, args=["back_history"])
+        if st.button(":back:"):
+            del st.session_state.view_history
+            st.experimental_rerun()
     instance.compare_history()
 
 def edit_instance():
@@ -114,24 +92,26 @@ def edit_instance():
         st.error(st.session_state.error, icon="🚨")
     # Init instance
     instance = st.session_state.edit_instance
-    # Init users
-    users = Users()
-    if not instance.user:
-        instance.user = users.list()[0]
     # Navigation
     with st.sidebar:
-        st.button(":back:", key="back_edit_instance", on_click=button_handler, args=["back_edit_instance"])
-        st.button(":floppy_disk:", key="save_instance", on_click=button_handler, args=["save_instance"])
+        if st.button(":back:"):
+            st.session_state.edit_instance.refresh()
+            del st.session_state.edit_instance
+            st.experimental_rerun()
+        if st.button(":floppy_disk:"):
+            st.session_state.edit_instance.save()
+            if st.session_state.edit_instance not in st.session_state.pbgui_instances.instances:
+                st.session_state.pbgui_instances.instances.append(st.session_state.edit_instance)
+            del st.session_state.edit_instance
+            st.experimental_rerun()
+        source_name = st.text_input('pbconfigdb by [Scud](%s)' % "https://pbconfigdb.scud.dedyn.io/", value="PBGUI", max_chars=16, key="name_input", help=pbgui_help.upload_pbguidb)
+        if st.button("Upload"):
+            upload_pbconfigdb(instance._config.config, instance.symbol, source_name)
+
+    instance.edit_base()
     col_1, col_2, col_3 = st.columns([1,1,1])
     with col_1:
-        instance.user = st.selectbox('User',users.list(), index = users.list().index(instance.user))
-    with col_2:
-        instance.symbol = st.selectbox('SYMBOL', instance.symbols, index=instance.symbols.index(instance.symbol))
-        st.button("Update Symbols from Exchange", key="update_symbols", on_click=button_handler, args=["update_symbols"])
-    with col_3:
-        instance.market_type = st.radio("MARKET_TYPE", instance.market_types)
-        with st.expander("Advanced configurations", expanded=False):
-            instance.ohlcv = st.checkbox("OHLCV", value=instance.ohlcv, key="live_ohlcv", help=pbgui_help.ohlcv)
+        with st.session_state.placeholder.expander("Advanced configurations", expanded=False):
             instance.leverage = st.number_input("LEVERAGE", min_value=2, max_value=20, value=instance.leverage, step=1, format="%d", key="live_lev", help=pbgui_help.lev)
             instance.assigned_balance = st.number_input("ASSIGNED_BALANCE", key="live_assigned_balance", min_value=0, step=500, value=instance.assigned_balance, help=pbgui_help.assigned_balance)
             instance.price_distance_threshold = round(st.number_input("PRICE_DISTANCE_THRESHOLD", key="live_price_distance_threshold", min_value=0.00, step=0.05, value=instance.price_distance_threshold, help=pbgui_help.price_distance_threshold),2)
@@ -157,4 +137,6 @@ elif 'edit_instance' in st.session_state:
     edit_instance()
 else:
     select_instance()
-    st.button(":heavy_plus_sign: Add Instance", key='add', on_click=button_handler, args=["add_instance"])
+    if st.button(":heavy_plus_sign: Add Instance"):
+        st.session_state.edit_instance = Instance()
+        st.experimental_rerun()
