aadfba146fea868e5c2e52283f173dd61a74965e
Manfred Seiler <mani@msei.ch>
2023-11-21 18:31:11 +0100

    bugfix PBRun

--- sync.py
+++ sync.py
@@ -5,9 +5,9 @@ def main():
     run = PBRun()
     while True:
         try:
-            run.rtd()
-            run.has_rtd('manibot3')
-            sleep(5)
+            run.alive()
+            run.has_remote()
+#            sleep(5)
         except Exception as e:
             print(f'Something went wrong, but continue {e}')
             sleep(5)
