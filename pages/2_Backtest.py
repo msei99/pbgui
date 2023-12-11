43b5f3612c1202fe1a55ac30333103f83fea411b
Manfred Seiler <mani@msei.ch>
2023-12-11 20:22:25 +0100

    V0.81 - bug fixes

--- pages/2_Backtest.py
+++ pages/2_Backtest.py
@@ -91,7 +91,7 @@ def bt_queue():
             'Balance': bt.sb,
             'Delete': False,
         })
-    selected = st.data_editor(data=d, width=None, height=None, use_container_width=True, key=f'editor_{st.session_state.ed_bt_key}', hide_index=None, column_order=None, column_config=column_config, disabled=['User','Symbol','Exchange','Start','End','Balance'])
+    selected = st.data_editor(data=d, width=None, height=(len(my_btq.items)+1)*36, use_container_width=True, key=f'editor_{st.session_state.ed_bt_key}', hide_index=None, column_order=None, column_config=column_config, disabled=['User','Symbol','Exchange','Start','End','Balance'])
     for line in selected:
         if line["Run"]:
             if not my_btq.items[line["id"]].is_running() and not my_btq.items[line["id"]].is_finish():
@@ -111,6 +111,7 @@ def bt_queue():
                 'id': line["id"],
                 'log': my_btq.items[line["id"]].load_log()})
             st.session_state.ed_bt_key += 1
+            
             st.experimental_rerun()
         if line["Config"]:
             st.session_state.bt_config = ({
