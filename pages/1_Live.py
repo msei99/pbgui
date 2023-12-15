987959da7067d54fbe7bb993a3dc4b14cefc8808
Manfred Seiler <mani@msei.ch>
2023-12-15 18:05:11 +0100

    fixes

--- pages/1_Live.py
+++ pages/1_Live.py
@@ -232,12 +232,15 @@ def select_instance():
                 st.experimental_rerun()
             if "Delete" in ed["edited_rows"][row]:
                 if not "confirm" in st.session_state:
-                    st.session_state.confirm_text = f':red[Delete selected instance ?]'
+                    st.session_state.confirm_text = f':red[Delete selected instance ({instances.instances[row].user} {instances.instances[row].symbol} {instances.instances[row].market_type})?]'
                     st.session_state.confirm = False
                     st.experimental_rerun()
                 elif "confirm" in st.session_state:
                     if st.session_state.confirm:
                         instances.remove(instances.instances[row])
+                        if PBStat().is_running():
+                            PBStat().stop()
+                            PBStat().run()
                         del st.session_state.confirm
                         del st.session_state.confirm_text
                         st.experimental_rerun()
