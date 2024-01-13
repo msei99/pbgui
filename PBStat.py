14c0e864592145e7557c9a7b18b383588599ec14
Manfred Seiler <mani@msei.ch>
2024-01-13 15:36:53 +0100

    added pidfiles

--- PBStat.py
+++ PBStat.py
@@ -1,6 +1,7 @@
 import psutil
 import subprocess
 import sys
+import os
 from pathlib import Path, PurePath
 from time import sleep
 from io import TextIOWrapper
@@ -10,17 +11,31 @@ from Instance import Instances
 class PBStat(Instances):
     def __init__(self):
         super().__init__()
+        pbgdir = Path.cwd()
+        self.piddir = Path(f'{pbgdir}/data/pid')
+        if not self.piddir.exists():
+            self.piddir.mkdir(parents=True)
+        self.pidfile = Path(f'{self.piddir}/pbstat.pid')
+        self.my_pid = None
 
     def run(self):
         if not self.is_running():
             pbgdir = Path.cwd()
             cmd = [sys.executable, '-u', PurePath(f'{pbgdir}/PBStat.py')]
             subprocess.Popen(cmd, stdout=None, stderr=None, cwd=pbgdir, text=True, start_new_session=True)
+            count = 0
+            while True:
+                if count > 5:
+                    print(f'{datetime.now().isoformat(sep=" ", timespec="seconds")} Error: Can not start PBStat')
+                sleep(1)
+                if self.is_running():
+                    break
+                count += 1
 
     def stop(self):
         if self.is_running():
             print(f'{datetime.now().isoformat(sep=" ", timespec="seconds")} Stop: PBStat')
-            self.pid().kill()
+            psutil.Process(self.my_pid).kill()
 
     def restart(self):
         if self.is_running():
@@ -28,18 +43,24 @@ class PBStat(Instances):
             self.run()
 
     def is_running(self):
-        if self.pid():
-            return True
+        self.load_pid()
+        try:
+            if self.my_pid and psutil.pid_exists(self.my_pid) and any(sub.lower().endswith("pbstat.py") for sub in psutil.Process(self.my_pid).cmdline()):
+                return True
+        except psutil.NoSuchProcess:
+            pass
         return False
 
-    def pid(self):
-        for process in psutil.process_iter():
-            try:
-                cmdline = process.cmdline()
-            except psutil.AccessDenied:
-                continue
-            if any("PBStat.py" in sub for sub in cmdline):
-                return process
+    def load_pid(self):
+        if self.pidfile.exists():
+            with open(self.pidfile) as f:
+                pid = f.read()
+                self.my_pid = int(pid) if pid.isnumeric() else None
+
+    def save_pid(self):
+        self.my_pid = os.getpid()
+        with open(self.pidfile, 'w') as f:
+            f.write(str(self.my_pid))
 
     def fetch_all(self):
         self.fetch_status()
@@ -66,7 +87,13 @@ def main():
     sys.stdout = TextIOWrapper(open(logfile,"ab",0), write_through=True)
     sys.stderr = TextIOWrapper(open(logfile,"ab",0), write_through=True)
     print(f'{datetime.now().isoformat(sep=" ", timespec="seconds")} Start: PBStat')
-    run = PBStat()
+    stat = PBStat()
+    if stat.is_running():
+        sys.stdout = sys.__stdout__
+        sys.stderr = sys.__stderr__
+        print(f'{datetime.now().isoformat(sep=" ", timespec="seconds")} Error: PBStat already started')
+        exit(1)
+    stat.save_pid()
     trade_count = 0
     while True:
         try:
@@ -76,11 +103,11 @@ def main():
                     sys.stdout = TextIOWrapper(open(logfile,"ab",0), write_through=True)
                     sys.stderr = TextIOWrapper(open(logfile,"ab",0), write_through=True)
             if trade_count%5 == 0:
-                run.fetch_all()
+                stat.fetch_all()
             else:
-                run.fetch_status()
+                stat.fetch_status()
             trade_count += 1
-            if len(run.instances) < 20:
+            if len(stat.instances) < 20:
                 sleep(60)
         except Exception as e:
             print(f'Something went wrong, but continue {e}')
