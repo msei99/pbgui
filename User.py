6741a12c7bc1523779799c2df40b33274468e8a1
mani <mani@msei.ch>
2024-11-11 21:29:40 +0100

    Bugfix load users, if only pb7 is configured

--- User.py
+++ User.py
@@ -133,7 +133,10 @@ class Users:
         try:
             if Path(self.api7_path).exists():
                 with Path(self.api7_path).open(encoding="UTF-8") as f:
-                    users.update(json.load(f))
+                    if users:
+                        users.update(json.load(f))
+                    else:
+                        users = json.load(f)
         except Exception as e:
             print(f'{self.api7_path} is corrupted {e}')
         for user in users:
