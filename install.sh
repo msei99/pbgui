0a47386d387da3ceae493245215483421ec03738
mani <mani@msei.ch>
2024-12-09 20:47:01 +0100

    Update readme

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+# echo Linux release
+lsb_release -i
+
+# git clone https://github.com/enarjord/passivbot.git pb6
+
+# venv=~/software/venv_pbgui       #Path to python venv
+# pbgui=~/software/pbgui          #path to pbgui installation
+
+# source ${venv}/bin/activate
+# cd ${pbgui}
+# python PBRun.py &
+# python PBRemote.py &
+# python PBStat.py &
+# python PBData.py &
+
+# curl -sfL https://get.k3s.io | sudo -E K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--kubelet-arg "container-log-max-files=4" --kubelet-arg
