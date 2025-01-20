2afb8003a5f4d6c63d711ecf13d2e4f93ca7a84d
mani <mani@msei>
2025-01-20 17:59:46 +0100

    Bugfix PBRun when user ends with same string

--- PBRun.py
+++ PBRun.py
@@ -604,7 +604,7 @@ class RunV7():
             except psutil.AccessDenied:
                 pass
             if any(self.user in sub for sub in cmdline) and any("main.py" in sub for sub in cmdline):
-                if cmdline[-1].endswith(f'{self.user}/config.json') or cmdline[-1].endswith(f'{self.user}\config.json'):
+                if cmdline[-1].endswith(f'/{self.user}/config.json') or cmdline[-1].endswith(f'\{self.user}\config.json'):
                     self.monitor.start_time = process.create_time()
                     self.monitor.memory = process.memory_info()
                     self.monitor.cpu = process.cpu_percent()
