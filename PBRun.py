c912581dc8905eeeadfc8c2066e03803c56cf160
mani <mani@msei>
2025-02-02 00:55:12 +0100

    add approved_coins to  dynamic_ignore for v6 multi

--- PBRun.py
+++ PBRun.py
@@ -544,6 +544,7 @@ class RunMulti():
                 self._multi_config["default_config_path"] = f'{self.path}/default.json'
         if self.dynamic_ignore is not None:
             self._multi_config["ignored_symbols"] = self.dynamic_ignore.coindata.ignored_coins
+            self._multi_config["approved_symbols"] = self.dynamic_ignore.coindata.approved_coins
         run_config = hjson.dumps(self._multi_config)
         config_file = Path(f'{self.path}/multi_run.hjson')
         with open(config_file, "w", encoding='utf-8') as f:
