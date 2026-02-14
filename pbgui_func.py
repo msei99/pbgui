f1c89f70643ec3fac97441a506f91bd156576adb
mani <mani@msei>
2026-02-14 23:11:09 +0100

    Release v1.55

--- pbgui_func.py
+++ pbgui_func.py
@@ -173,7 +173,7 @@ def set_page_config(page : str = "Start"):
         initial_sidebar_state="expanded",
         menu_items={
             'Get help': 'https://github.com/msei99/pbgui/#readme',
-            'About': "Passivbot GUI v1.54 [![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/Y8Y216Q3QS)"
+            'About': "Passivbot GUI v1.55 [![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/Y8Y216Q3QS)"
         }
     )
     # Check VPS Errors
